db4f8380f2741a31616c726f41f07603b6d78b45
18thompsona <milkman9985@gmail.com>
2024-12-24 15:57:54 -0500

    Added ability to search using the searchbar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import WeatherAPI from "./WeatherAPI";
 
-let location = "Edinboro";
+const SearchForm = document.querySelector(".searchform");
 
+SearchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const SearchTerm = document.querySelector("#searchbox").value;
+    console.log(SearchTerm);
+    SearchWeather(SearchTerm);
+    document.querySelector("#searchbox").value = "";
+})
 
-const weatherData = await WeatherAPI.GetData(location);
-console.log(weatherData);
+const location = document.querySelector(".location");
+const description = document.querySelector(".description");
+const temp = document.querySelector(".temp");
 
+async function SearchWeather(city) {
+    try{
+        const weatherData = await WeatherAPI.GetData(city);
+        console.log(weatherData);
+        DisplayInfo(weatherData);
+    }
+    catch{
+        console.error("Error Fetching Data!");
+        DisplayError();
+    }
+}
+
+function DisplayInfo(weatherData){
+        location.textContent = weatherData.location;
+        description.textContent = weatherData.description;
+        temp.textContent = weatherData.temperature + '°F';
+}
+
+function DisplayError(){
+    location.textContent = "City Not Found!"
+    description.textContent = "";
+    temp.textContent = "";
+}
